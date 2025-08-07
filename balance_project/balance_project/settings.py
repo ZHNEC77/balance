43dd71e11a1b5114eba07e68af08a4ff9fbdaca0
ZHNEC77 <vadim030714@gmail.com>
2025-08-08 02:28:57 +0300

    add users app

--- balance_project/balance_project/settings.py
+++ balance_project/balance_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # my_app
+    'users.apps.UsersConfig',
 
     # DRF
     'rest_framework',
@@ -136,3 +137,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
